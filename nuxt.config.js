6db7820aade2c55fa27a14ad13650d2691369526
Koala <ramida@5lab.co>
2020-09-14 22:48:29 +0700

    add font

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,14 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap'
+      }
+    ]
   },
 
   /*
